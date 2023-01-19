9775125baebdd7fed3c5b95d3246bd77bb37d553
Ciro <ciroma809@gmail.com>
2023-01-19 10:20:19 +0100

    update communication tests

--- LevenshteinAutomataProtocol/test/communication_test.py
+++ LevenshteinAutomataProtocol/test/communication_test.py
@@ -1,4 +1,5 @@
 from LevenshteinAutomataProtocol.src.communication import ServerSocket, ClientSocket
+from phe import paillier
 
 m1 = {
     "test": "prova",
@@ -11,15 +12,21 @@ m2 = {
     "arr": b'fasddklsdfjk'
 }
 
+pk, sk = paillier.generate_paillier_keypair()
+
 
 def test_server():
     socket = ServerSocket(9000)
     data = socket.recv()
     assert data == m1
-    socket.send(m2)
+    n = socket.send_wait(m2)
+    pk2 = paillier.PaillierPublicKey(n)
+    socket.send(pk2.encrypt(10))
 
 
 def test_client():
     socket = ClientSocket('localhost', 9000)
     data = socket.send_wait(m1)
     assert data == m2
+    c = socket.send_wait(pk.n)
+    assert sk.decrypt(c) == 10
